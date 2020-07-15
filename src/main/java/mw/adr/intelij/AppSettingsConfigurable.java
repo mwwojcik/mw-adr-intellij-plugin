393129dbfa0e3f173c6358100c7a4e90a4cd0381
mwwojcik <mw.wojcik@gmail.com>
2020-07-15 08:40:30 +0200

    naming conventions

--- src/main/java/mw/adr/intelij/AppSettingsConfigurable.java
+++ src/main/java/mw/adr/intelij/AppSettingsConfigurable.java
@@ -17,7 +17,7 @@ public class AppSettingsConfigurable implements Configurable {
   @Nls(capitalization = Nls.Capitalization.Title)
   @Override
   public String getDisplayName() {
-    return "SDK: ADR Plugin Settings";
+    return "ADR/ADL Plugin Settings";
   }
 
   @Override
